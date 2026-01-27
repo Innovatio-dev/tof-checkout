01b7fe0bf57a3034266a868092d244b335457f8a
Ivan Tovar <info@innovatio.dev>
2026-01-27 17:50:47 -0600

    Add custom TOF radio item component with styled label and hidden native input

---
+++ components/custom/tof-radio-item.tsx
@@ -0,0 +1,31 @@
+import { RadioGroupItem } from "@/components/ui/radio-group"
+import { cn } from "@/lib/utils"
+
+type TofRadioItemProps = {
+  id: string
+  value: string
+  label: string
+}
+
+const TofRadioItem = ({ id, value, label }: TofRadioItemProps) => {
+  return (
+    <div>
+      <RadioGroupItem value={value} id={id} className="sr-only peer" />
+      <label
+        htmlFor={id}
+        className={cn(
+          "flex h-11 items-center justify-center rounded-md border cursor-pointer",
+          "border-white/10 bg-white/10 px-4 text-sm font-semibold text-white/70",
+          "transition-colors hover:border-white/40 hover:bg-white/15",
+          "peer-data-[state=checked]:border-white",
+          "peer-data-[state=checked]:bg-white peer-data-[state=checked]:text-black",
+          "peer-data-[state=checked]:hover:bg-white peer-data-[state=checked]:hover:text-black"
+        )}
+      >
+        {label}
+      </label>
+    </div>
+  )
+}
+
+export default TofRadioItem
